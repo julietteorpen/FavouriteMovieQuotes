22a927f9992ce2c3779c3c987312fff0d06d8df6
neprojo <53294140+neprojo@users.noreply.github.com>
2024-02-16 15:45:13 +0000

    separate styling file

---
+++ src/components/AddQuoteStyles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subheader: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: "100%",
+  },
+});
