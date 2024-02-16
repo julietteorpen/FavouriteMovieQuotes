096e7643ef40ef4e3728e04e8cf58bbfb213060f
neprojo <53294140+neprojo@users.noreply.github.com>
2024-02-16 15:45:03 +0000

    render quotes in list

--- src/components/QuotesList.js
+++ src/components/QuotesList.js
@@ -1,22 +1,61 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
-import { UseDispatch, useSelector } from "react-redux";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { EvilIcons } from "@expo/vector-icons";
+import { deleteQuote } from "../slices/quotes";
 
 // Component to display an individual quote
-const QuoteCard = ({ quote }) => {
-  return <View style={styles.card}>{<Text>{quote}</Text>}</View>;
+const QuoteCard = ({ quote, score, rank }) => {
+  const dispatch = useDispatch();
+  console.log(16, "quote,score", quote, score);
+  return (
+    <View style={styles.card}>
+      <Text>{rank}</Text>
+      <Text>{quote}</Text>
+      <Text>{score}</Text>
+
+      <TouchableOpacity onPress={() => dispatch(deleteQuote(quote))}>
+        <EvilIcons name="trash" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 // Component to render a list of QuoteCards
 const QuotesList = () => {
   const { quotes } = useSelector((state) => state.quotes);
-  console.log(18, "quotesList", quotes);
+  console.log(30, "quotes", quotes);
   return (
     <FlatList
       data={quotes}
-      renderItem={({ item }) => {
+      contentContainerStyle={{ alignItems: "center" }}
+      ListHeaderComponent={() => (
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            width: "80%",
+            paddingBottom: 10,
+            paddingRight: "20%",
+          }}
+        >
+          <Text>Rank</Text>
+          <Text>Quote</Text>
+          <Text>Score</Text>
+        </View>
+      )}
+      renderItem={({ index, item }) => {
         console.log(23, "item", item.quote);
-        return <QuoteCard quote={item.quote} />;
+        return (
+          <QuoteCard quote={item.quote} score={item.score} rank={index + 1} />
+        );
       }}
       keyExtractor={(item, index) => index.toString()}
     />
@@ -30,7 +69,10 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 5,
     borderWidth: 1,
+    width: "80%",
     borderColor: "#ccc",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
 
